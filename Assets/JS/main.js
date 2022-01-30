78b1e6c6fa85a772487442b9933b0b7c55f0ef62
MonaMahmoud <monamohamedfahmy@gmail.com>
2022-01-31 04:03:54 +1100

    Added comments to main.js

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,30 +1,36 @@
 var cityData;
 var cityWeather;
-//var storedCityName;
-
-var storedWeather = JSON.parse(localStorage.getItem("WeatherSearch"));
-var storedCities = JSON.parse(localStorage.getItem("CityNames"));
-
+// var storedWeather = JSON.parse(localStorage.getItem("WeatherSearch"));
+// var storedCities = JSON.parse(localStorage.getItem("CityNames"));
+var storedWeather ;
+var storedCities;
+//function to display get previoulsy stored values from local storage
 function loadSearchHistory(){
-
+    //fetch values stored in local storage
+    storedWeather = JSON.parse(localStorage.getItem("WeatherSearch"));
+    storedCities = JSON.parse(localStorage.getItem("CityNames"));
+    //if there are previously stored values 
     if(storedWeather){
         var i;
+        //loop on all stored values
         for(i=0;i<storedWeather.length;i++){
-            //console.log(storedWeather[i]);
+            //create a button for each value
             var result = document.createElement("button");
+            //set data attribute equals to array index of the city represented by the button
             result.setAttribute("data-order",i);
-
+            //set text on button to city name
             result.textContent = storedCities[i].name;
-            //should be city name//////
-            //result.textContent = storedWeather[i].current.temp;
+            //set button styling
             result.classList.add("btn");
             result.classList.add("btn-success");
+            //add the button to HTML
             document.getElementById("history").appendChild(result);
+            //add event listener for the button click
             result.addEventListener("click",function(event){
-                console.log(event.target);
-                console.log(event.target.getAttribute("data-order"));
+                //get the data for the city represented by the button using the data attribute that was set when creating the button
                 cityWeather = storedWeather[event.target.getAttribute("data-order")];
                 cityData = storedCities[event.target.getAttribute("data-order")];
+                //display the weather data 
                 displayWeather();
             });
         }
@@ -32,8 +38,9 @@ function loadSearchHistory(){
 
 }
 
+//function to get city weather using its name
 function getCityWeatherFromName(name){
-    // cityName = name;
+    
     //construct request URL to get city coordinates from name
     var requestCityCoordinates = "https://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=022816ce4f8542d4f9f3d06e40efbb54";
     //call the server API to get city coordinates from name
@@ -48,27 +55,30 @@ function getCityWeatherFromName(name){
     });
 }
 
+//function to get weather data using latitude and longitude
 function getCityWeather(lat, lon){
-    //if it's not a null city
-        var requestCityWeather = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=022816ce4f8542d4f9f3d06e40efbb54&units=metric";    
+    //construct URL to get weather data    
+    var requestCityWeather = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=022816ce4f8542d4f9f3d06e40efbb54&units=metric";    
+        //call the API to get the weather data
         fetch(requestCityWeather)
         .then(function (response){
             return response.json();
         })
         .then(function(weather){
             cityWeather = weather;
-            //update the GUI with the cityWeather
+            //update the GUI with the data returned by the API and stored in cityWeather
             displayWeather();
         });
 }
 
+//function to update GUI with weather data for current and 5 day forecast
 function displayWeather(){
-    console.log(cityWeather)
-
+//loop to show 5 day divs
     const nodeList = document.querySelectorAll(".weatherInfo");
 for (let i = 0; i < nodeList.length; i++) {
     nodeList[i].style.display = "block";
 }
+//display data from current and 5 days forecast into corresponding div
     var uvIndex;
     document.getElementById("city").textContent = cityData.name;
     document.getElementById("temp").textContent = "Temperature: "+cityWeather.current.temp;
@@ -76,6 +86,8 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("humidity").textContent = "Humidity: "+cityWeather.current.humidity;
     document.getElementById("uv").textContent = "Uv Index: "+cityWeather.current.uvi;
     uvIndex = parseFloat(cityWeather.current.uvi);
+
+    //change background value based on UV index value
     if(uvIndex>11)
     document.getElementById("uv").style.backgroundColor = "violet";
     else if(uvIndex>8)
@@ -96,6 +108,7 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("uv1").textContent = "UV Index: "+cityWeather.daily[1].uvi;
 
     uvIndex = parseFloat(cityWeather.daily[1].uvi);
+    //change background value based on UV index value
     if(uvIndex>11)
     document.getElementById("uv1").style.backgroundColor = "violet";
     else if(uvIndex>8)
@@ -118,6 +131,7 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("uv2").textContent = "UV Index: "+cityWeather.daily[2].uvi;
 
     uvIndex = parseFloat(cityWeather.daily[2].uvi);
+    //change background value based on UV index value
     if(uvIndex>11)
     document.getElementById("uv2").style.backgroundColor = "violet";
     else if(uvIndex>8)
@@ -140,6 +154,7 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("uv3").textContent = "UV Index: "+cityWeather.daily[3].uvi;
 
     uvIndex = parseFloat(cityWeather.daily[3].uvi);
+    //change background value based on UV index value
     if(uvIndex>11)
     document.getElementById("uv3").style.backgroundColor = "violet";
     else if(uvIndex>8)
@@ -162,6 +177,7 @@ for (let i = 0; i < nodeList.length; i++) {
 
 
     uvIndex = parseFloat(cityWeather.daily[4].uvi);
+    //change background value based on UV index value
     if(uvIndex>11)
     document.getElementById("uv4").style.backgroundColor = "violet";
     else if(uvIndex>8)
@@ -182,6 +198,7 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("uv5").textContent = "UV Index: "+cityWeather.daily[5].uvi;
 
     uvIndex = parseFloat(cityWeather.daily[5].uvi);
+    //change background value based on UV index value
     if(uvIndex>11)
     document.getElementById("uv5").style.backgroundColor = "violet";
     else if(uvIndex>8)
@@ -197,10 +214,7 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("date5").textContent = new Date(cityWeather.daily[5].dt*1000).toDateString();
 
 
-    // if(parseFloat(cityWeather.daily[5].uvi)>11)
-    // getElementById("UVI5").src = "./Assets/images/ultraviolet.jfif";
-
-    
+    //change the icons based on returned icon codes
     document.getElementById("icon").src = "https://openweathermap.org/img/wn/"+cityWeather.daily[0].weather[0].icon+".png";
     document.getElementById("icon1").src = "https://openweathermap.org/img/wn/"+cityWeather.daily[1].weather[0].icon+".png";
     document.getElementById("icon2").src = "https://openweathermap.org/img/wn/"+cityWeather.daily[2].weather[0].icon+".png";
@@ -208,25 +222,36 @@ for (let i = 0; i < nodeList.length; i++) {
     document.getElementById("icon4").src = "https://openweathermap.org/img/wn/"+cityWeather.daily[4].weather[0].icon+".png";
     document.getElementById("icon5").src = "https://openweathermap.org/img/wn/"+cityWeather.daily[5].weather[0].icon+".png";
 
-    // var storedWeather;
-    // localStorage.getItem("WeatherSearch",storedWeather);
-
     
 
+    
+    //there are no stored values in local storage (first search) so create a new array
     if(!storedWeather){
-        //console.log("new array");
-        //console.log(storedWeather);
         storedWeather = new Array();
         storedCities = new Array();
     }
-    storedWeather.push(cityWeather);
+
+    var repeated = false;
+    var j;
+    //for loop to check if this city has been stored before in local storage to avoid redundancy
+    for (j=0;j<storedWeather.length;j++){
+        if(storedCities[j].name == cityData.name){
+            repeated = true;
+            break;
+        }
+    }
+    //the city is not repeated so store the value in locak storage on top of old values, if any
+    if(!repeated){
+        storedWeather.push(cityWeather);
     storedCities.push(cityData);
     //store values into local storage
     localStorage.setItem("WeatherSearch",JSON.stringify(storedWeather));
     localStorage.setItem("CityNames",JSON.stringify(storedCities));
+    }
+    
 
 }
-
+//add event listener to search button to start the whole process
 document.getElementById("searchBtn").addEventListener("click",function(){
     var cityName = document.getElementById("cityInput").value;
     getCityWeatherFromName(cityName);
